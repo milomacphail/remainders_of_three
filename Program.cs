26c72e6e66770d00798f33e92975c8e3178c0861
milomacphail <milomacphail@gmail.com>
2019-04-08 18:22:17 -0400

    Remainders function passes

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace _remainders
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Please enter a number.");
+            int entryNumber = int.Parse(Console.ReadLine());
+
+            int remainderNumber = 0;
+
+            for(int i= 0; i < entryNumber; i++)
+            {
+                if (i%3 == 0)
+                {
+                    remainderNumber++;
+                }
+            }
+
+            Console.WriteLine(remainderNumber);
+        }
+    }
+}
